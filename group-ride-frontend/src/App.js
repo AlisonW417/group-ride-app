d1ec5fdd237e882ef904f0897110f997fc9f89dc
Alison Williams <apw5011@gmail.com>
2021-08-05 10:10:05 -0400

    update to app.js

--- group-ride-frontend/src/App.js
+++ group-ride-frontend/src/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 import Navigation from './components/Navigation';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <Container>
         <Navigation />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Container>
+          <div><h1>Welcome to the Group Ride App</h1></div>
+        </Container>
+      </Container>
     );
   } 
 }
