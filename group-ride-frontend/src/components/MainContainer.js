3bb1c668ee28bcc46245c6d17b779abf5e82cdd7
Alison Williams <apw5011@gmail.com>
2021-08-06 11:27:13 -0400

    added sign up form

--- group-ride-frontend/src/components/MainContainer.js
+++ group-ride-frontend/src/components/MainContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import Signup from './Signup';
 
 const MainContainer = () => {
     return (
         <Container>
             <div>
                 <h1>Welcome to the Group Ride App</h1>
+                <Signup />
             </div>
         </Container>
     )
--- group-ride-frontend/src/components/Signup.js
+++ group-ride-frontend/src/components/Signup.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+
 
 const Signup = () => {
     return (
-        <div></div>
+        <Container>
+            <div>
+                <h2>Sign up for Group Ride to start logging your rides and interacting with other users</h2>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="email" placeholder="Create a username" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Sign up
+                    </Button>
+                </Form>
+            </div>
+        </Container>
     )
 }
 
