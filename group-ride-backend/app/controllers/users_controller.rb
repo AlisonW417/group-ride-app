37f82b921057cf0407d51dcb7f8c3cbd9ed5b734
Alison Williams <apw5011@gmail.com>
2021-08-16 17:20:41 -0400

    added and test user serializer

--- group-ride-backend/app/controllers/users_controller.rb
+++ group-ride-backend/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id 
-            render json: @user
+            render json: UserSerializer.new(@user).serializable_hash.to_json
         else 
             response = {
                 error: @user.errors.full_messages.to_sentence
