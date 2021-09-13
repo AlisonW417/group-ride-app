e5e1f76b53e6ef35c5c0dce57c2f0b9fa56855e8
Alison Williams <apw5011@gmail.com>
2021-09-13 16:20:35 -0400

    changed config to persist sessions

--- group-ride-backend/config/routes.rb
+++ group-ride-backend/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#detroy'
+  delete '/logout', to: 'sessions#destroy'
   get '/get_current_user', to: 'sessions#get_current_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
