a2ba2eac6104d417ff4fb96a3f5afacda34657af
Alison Williams <apw5011@gmail.com>
2021-09-27 14:47:33 -0400

    added create ride method to api

--- group-ride-backend/app/controllers/rides_controller.rb
+++ group-ride-backend/app/controllers/rides_controller.rb
@@ -1,2 +1,19 @@
 class RidesController < ApplicationController
+    def create 
+        #byebug
+        @ride = current_user.rides.build(ride_params)
+        if @ride.save 
+            render json: @ride
+        else 
+            response = {
+                error: @ride.errors.full_messages.to_string
+            }
+            render json: response
+        end 
+    end 
+
+    private 
+    def ride_params
+        params.require(:ride).permit(:name, :starting_location, :starting_time, :date, :level, :distance, :description, :user_id)
+    end 
 end
