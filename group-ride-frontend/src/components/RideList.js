c521d11c3107c3a1eff37d5fa5cca2165658ea95
Alison Williams <apw5011@gmail.com>
2021-10-04 12:59:42 -0400

    updated ride list component

--- group-ride-frontend/src/components/RideList.js
+++ group-ride-frontend/src/components/RideList.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import RideCard from './RideCard';
+import { Container, CardDeck } from 'react-bootstrap';
 
 const RideList = ({rides}) => {
+    const rideCards = rides.map(ride => <RideCard ride={ride} key={ride.id} /> )
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <h1>All the Rides</h1>
+            <CardDeck>
+                { rideCards.length > 0 ? rideCards : null }
+            </CardDeck>
+        </Container>
     )
 }
 
